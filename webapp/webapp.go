afea0dd67a5bf5fa6cc5946088f541b615ddd485
Bryan McKelvey <bryan.mckelvey@gmail.com>
2020-02-28 17:08:23 +0900

    Add common error types

--- webapp/webapp.go
+++ webapp/webapp.go
@@ -1,7 +1,10 @@
 package webapp
 
 import (
+	"fmt"
 	"net/http"
+	"runtime"
+	"runtime/debug"
 	"time"
 
 	log "github.com/sirupsen/logrus"
@@ -26,4 +29,26 @@ func Serve(app WebApp) {
 
 	log.Infof("starting server on %s", port)
 	log.Fatal(srv.ListenAndServe())
+}
+
+func ServerError(w http.ResponseWriter, err error) {
+	trace := fmt.Sprintf("%s\n%s", err.Error(), debug.Stack())
+	if _, file, line, ok := runtime.Caller(2); ok {
+		log.WithFields(log.Fields{
+			"file": file,
+			"line": line,
+		}).Error(trace)
+	} else {
+		log.Error(trace)
+	}
+
+	http.Error(w, http.StatusText(http.StatusInternalServerError), http.StatusInternalServerError)
+}
+
+func ClientError(w http.ResponseWriter, status int) {
+	http.Error(w, http.StatusText(status), status)
+}
+
+func NotFound(w http.ResponseWriter) {
+	ClientError(w, http.StatusNotFound)
 }
