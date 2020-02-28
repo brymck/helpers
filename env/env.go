d84161a7f0d933448c38eded00b02a902618c7c8
Bryan McKelvey <bryan.mckelvey@gmail.com>
2020-02-28 16:45:48 +0900

    Add GetPort

--- env/env.go
+++ env/env.go
@@ -5,7 +5,7 @@ import (
 	"os"
 )
 
-// MustGetEnv retrieves the value of the environment variable named by the key.
+// MustGetEnv retrieves the value of the env variable named by the key.
 // It returns the value and panics if the value is unset or blank.
 func MustGetEnv(key string) string {
 	value := os.Getenv(key)
@@ -15,10 +15,21 @@ func MustGetEnv(key string) string {
 	return value
 }
 
-// MustMapEnv retrieves the value of the environment variable named by the key
+// MustMapEnv retrieves the value of the env variable named by the key
 // and assigns it to the target string reference. It panics if the value is
 // unset or blank.
 func MustMapEnv(target *string, key string) {
 	value := MustGetEnv(key)
 	*target = value
+}
+
+// GetPort retrieves the port from the PORT environment variable, falling back
+// to the provided default value if that value is unset.
+func GetPort(defaultPort string) string {
+	envPort := os.Getenv("PORT")
+	if envPort == "" {
+		return defaultPort
+	} else {
+		return envPort
+	}
 }
